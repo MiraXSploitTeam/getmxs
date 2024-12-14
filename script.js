6a2a8257f450e964db7f3313ea666fbeeef12af0
MiraXSploitTeam <rafsankabbo13@gmail.com>
2024-12-14 15:25:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const glowingBall = document.getElementById('glowingBall');
 
-document.addEventListener('mousemove', (e) => {
-    const x = e.pageX;
-    const y = e.pageY;
-    glowingBall.style.transform = `translate(${x}px, ${y}px)`;
-});
